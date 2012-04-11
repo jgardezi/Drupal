72205191f69db660e1f8e0e038052e57df5635c1
Javed Gardezi <jgardezi@gmail.com>
2012-04-11 23:55:05 +1000

    Backup for changing from ubuntu to suse

---
+++ sites/all/themes/smbs/html.tpl.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="<?php print $language->language; ?>">
+    
+<head profile="<?php print $grddl_profile; ?>">
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
---
+++ sites/all/themes/smbs/node.tpl.php
@@ -0,0 +1,31 @@
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> newsarea clearfix"<?php print $attributes; ?>>
+
+  <?php //print $user_picture; ?>
+
+  <?php //print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2 class="post_title"<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php //print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <p class="meta">
+      Posted <?php print $date; ?> by <?php print $name; ?> in <?php print $type; ?>
+    </p>
+  <?php endif; ?>
+
+  
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      krumo($content);
+      print render($content);
+    ?>
+  
+
+  <?php print render($content['links']); ?>
+
+  <?php print render($content['comments']); ?>
+
+</article>
