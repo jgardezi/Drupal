2ed2c283c3d0959f5e5eac893f82acddb0d8f6f8
Javed Gardezi <jgardezi@gmail.com>
2012-04-20 00:40:35 +1000

    Added taxonomy module in drupal.

--- sites/all/themes/smbs/page.tpl.php
+++ sites/all/themes/smbs/page.tpl.php
@@ -1,35 +1,41 @@
 <div id="wrapper" class="container_16">
 
-    <header id="mainheader" class="grid_16">
-        <section id="logo" class="grid_11 alpha">
-            <h1><a href="#">Single Molecule <span>Bioscience</span></a></h1>
-        </section>
-        <aside id="logo_unsw" class="grid_5 omega">
-            <h1>@unsw</h1>
-        </aside>
+	<header id="mainheader" class="grid_16">
+		<section id="logo" class="grid_11 alpha">
+			<h1>
+				<a href="#">Single Molecule <span>Bioscience</span></a>
+			</h1>
+		</section>
+		<aside id="logo_unsw" class="grid_5 omega">
+			<h1>@unsw</h1>
+		</aside>
         <?php if ($main_menu): ?>
             <nav id="mainNav" class="grid_16">
-                <h1>Main page navigation</h1>
+			<h1>Main page navigation</h1>
                 <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'siteNav', 'class' => array('Black')),)); ?>
-            </nav> <!-- /.section, /#navigation -->
+            </nav>
+		<!-- /.section, /#navigation -->
         <?php endif; ?>
     </header>
 
-    <section id="textArea" class="grid_16">
-
+	<section id="textArea" class="grid_16">
+    <?php if($secondary_menu): ?>
+                <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+                <?php endif; ?>
         <header id="newsHd" class="">
-            <nav id="pageNav" class="">
-                <hgroup>
-                    <h1>News page navigation</h1>
-                    <!-- <h2>Archive</h2>  -->
-                </hgroup>
-                <ul id="newsNav">
-                    <li><a href="#">Feb 2012</a></li>
-                    <li><a href="#">Jan 2012</a></li>
-                    <li><a href="#" class="last">Dec 2011</a></li>
-                </ul>
-            </nav>
-        </header>
+			<nav id="pageNav" class="">
+				<hgroup>
+					<h1>News page navigation</h1>
+					<!-- <h2>Archive</h2>  -->
+				</hgroup>
+
+				<ul id="newsNav">
+					<li><a href="#">Feb 2012</a></li>
+					<li><a href="#">Jan 2012</a></li>
+					<li><a href="#" class="last">Dec 2011</a></li>
+				</ul>
+			</nav>
+		</header>
 
 
         <?php print $messages; ?>
@@ -46,12 +52,15 @@
         <?php print render($page['content']); ?>
 
         <footer id="mainfooter" class="">
-            <div id="box">
-                <img width="15" height="15" alt="Developer" src="images/icons/developer.png"> Developed by <a href="http://javedweb.com/">Javed Gardezi</a> and <a href="#"> Samuel</a>
-            </div>
-        </footer>
+			<div id="box">
+				<img width="15" height="15" alt="Developer"
+					src="images/icons/developer.png"> Developed by <a
+					href="http://javedweb.com/">Javed Gardezi</a> and <a href="#">
+					Samuel</a>
+			</div>
+		</footer>
 
-    </section>
+	</section>
 
 
 </div>
